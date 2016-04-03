3bbdc12b1d3289c847b0a08cfd7d18ca3bce19ee
bartkow <bart.kowalik@blue-bart.com>
2016-04-03 06:15:00 -0400

    Create footer.php

---
+++ kod/footer.php
@@ -0,0 +1,22 @@
+<section class="footer-section">
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<center>  <b class="copyright">STOPKA</center>   </div>
+
+</div>
+</div>
+</section>
+
+
+
+<script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+    <!-- DATATABLE SCRIPTS  -->
+    <script src="assets/js/dataTables/jquery.dataTables.js"></script>
+    <script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
+      <!-- CUSTOM SCRIPTS  -->
+    <script src="assets/js/custom.js"></script>
+      
+    </body>
