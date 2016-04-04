04c2ce8358e6368481d32001036a3da3a1b26923
bartkow <bart.kowalik@blue-bart.com>
2016-04-04 15:03:43 -0400

    Update home.php

--- kod/home.php
+++ kod/home.php
@@ -5,7 +5,9 @@ require("./header.php");
 
 <b> Zalogowano poprawnie!<br><b>
 Nazwa uzytkownika: <b><?php echo $_SESSION['username'];?></b><br>
-Kod uprawnien: <b><?php echo useraccess($_SESSION['username']); ?></b>
+Kod uprawnien: <b><?php echo useraccess($_SESSION['username']); ?></b><br>
+
+Uprawnienia: <b><?php if(useraccess($_SESSION['username'])=='1'){ echo "Administrator"; } if(useraccess($_SESSION['username'])=='2'){ echo "Magazynier"; } ?></b><br>
 
 <br><br><a href="./logout.php">WYLOGUJ</a>
 <?
