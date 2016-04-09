5a1acde4bc5810b1bbee86988e882f97f4e8c9ee
bartkow <bart.kowalik@blue-bart.com>
2016-04-09 11:34:24 -0400

    Update login.php
    Zaktualizowano o wyswietlanie bledow oraz dodano GUI

--- kod/login.php
+++ kod/login.php
@@ -9,33 +9,62 @@ $db = dbConnect();
   } else {
 ?>
 
-<!-- KOD LOGOWANIA -->
 
-<? 
-  function form($error)
-  { if($error){ 	echo '<section class="login"> <center>'.$error.'</center></section>'; } else { echo ''; }}
-?> 
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Logowanie :: Komputerex</title>
+    
+    
+    <link rel="stylesheet" href="login/css/reset.css">
 
-<form id="login-form" accept-charset="utf-8" action="" method='POST'>
-<h1>Logowanie do systemu</h1>
-<input type="text" value="" placeholder="username" tabindex="20" name="username">
-  <div class="password-container">
-<input type="password" placeholder="password" tabindex="21" name="password">
+    <link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900'>
+<link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Montserrat:400,700'>
+<link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
 
+        <link rel="stylesheet" href="login/css/style.css">
+
+    
+    
+    
+  </head>
+
+  <body>
+
+    
+<div class="container">
+  <div class="info">
+    <h1>Komputerex</h1>
+  </div>
 </div>
-<button class="button submit" data-analytics="sign-in" type="submit" id="Submit" name="Submit">Zaloguj</button>
-  </section>
-  <br><br>
- <section class="login">
-<center><small>Stopka</small></center>
-</section>
+<div class="form">
+  <div class="thumbnail"><img src="login/images/k.png"/></div>
+ 
 
-<?
+
+
+  <form class="login-form" action="" method='POST'>
+    <input type="text" placeholder="username" name="username"/>
+    <input type="password" placeholder="password" name="password"/>
+    <button class="button submit" data-analytics="sign-in" type="submit" id="Submit" name="Submit">login</button>
+    
+  </form>
+</div>
+
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+        <script src="login/js/index.js"></script>
+
+    <p><center><h1>Flat Login Form</h1><span>Made with <i class="fa fa-heart"></i> by <a href="http://andytran.me">Andy Tran</a></span></center></p>
+    
+  <?
   
   if(isset($_REQUEST['Submit'])) {
     if(!$_POST['username'] || !$_POST['password']) {
-      $error = 'Blad: Wszystkie pola sa wymagane';
-      echo form($error);
+      ?>
+      <script type="text/javascript">alert("Wszystkie pola są wymagane");</script>
+      <?
     } else {
       if (login_check($_POST['username'],$_POST['password'])) {
         header('Location: home.php');
@@ -43,8 +72,11 @@ $db = dbConnect();
         $_SESSION["username"] = $_POST['username'];
         $_SESSION["password"] = $_POST['password'];
             } else {
-        $error = "Bledna nazwa uzytkownika, lub password";
-        echo form($error);
+       
+        ?>
+        <script type="text/javascript">alert("Błedna nazwa użytkownika, lub hasło");</script>
+        <?
+       
       }
     }
   } else {
@@ -67,7 +99,3 @@ $db = dbConnect();
 <?
 }
 ?>
-
-
-
-
