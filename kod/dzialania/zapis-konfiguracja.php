13fc927f20655e3316daf94fdd09a3fd5c03416d
bartkow <bart.kowalik@blue-bart.com>
2016-04-10 14:38:32 -0400

    Update zapis-konfiguracja.php

--- kod/dzialania/zapis-konfiguracja.php
+++ kod/dzialania/zapis-konfiguracja.php
@@ -1,1 +1,12 @@
+<?
+require('../include/functions.inc.php');
+$db = dbConnect();
+
+$nazwa = $_POST['nazwa'];
+$punkty = $_POST['punkty'];
+mysql_query("UPDATE `ustawienia` SET `nazwa` = '$nazwa', `punkty` = '$punkty' WHERE `ustawienia`.`id` =1;");
+?>
+
+
+<META HTTP-EQUIV="REFRESH" CONTENT="0; URL='../home.php?kg=001'"> 
 
